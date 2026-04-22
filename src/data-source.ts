3c9ca9e4df6c098a6915566fc28ef4c9a5c7fd1c
Rodrigo Linares <rodolinares19@gmail.com>
2026-04-22 16:45:14 -0500

    feat: config typeorm cli

---
+++ src/data-source.ts
@@ -0,0 +1,15 @@
+import { config } from 'dotenv'
+import { DataSource } from 'typeorm'
+
+config()
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/entities/*.ts'],
+  migrations: [__dirname + '/migrations/*.ts']
+})
